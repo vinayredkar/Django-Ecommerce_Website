6f07c2a6e508234a64157be45d2d31b461b817d8
vinayredkar <vinayredkar43@gmail.com>
2024-12-18 22:55:11 +0530

    new branch

--- ecommerce_website/settings.py
+++ ecommerce_website/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-# Default primary key field type
+git # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
