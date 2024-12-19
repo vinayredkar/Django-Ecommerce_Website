dfb94e60ba8eec85d5ead08c7be70ad6fd1bf952
Your Name <your.email@example.com>
2024-12-19 19:31:00 +0530

    changes in urls

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.store, name="store"),
-    path('cart/',views.store, name="cart"),
+    path('cart/',views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout")
 ]
